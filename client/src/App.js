e07d7c2e5f07b83a45957ac9934eccdfb528b870
huntsimon23 <hunt.simon23@gmail.com>
2019-07-19 01:05:36 -0400

    boilerplate okta code

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Security, ImplicitCallback } from '@okta/okta-react';
+import Home from './Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const config = {
+  issuer: 'https://dev-631231.okta.com/oauth2/default',
+  redirect_uri: window.location.origin + '/implicit/callback',
+  client_id: '{clientId}'
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Security issuer={config.issuer}
+                  client_id={config.client_id}
+                  redirect_uri={config.redirect_uri}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Route path='/' exact={true} component={Home}/>
+          <Route path='/implicit/callback' component={ImplicitCallback}/>
+        </Security>
+      </Router>
+    );
+  }
 }
 
 export default App;
