5e1cac81f332f7b152b6eb268c23d1c9ab271f5d
Kevin Stokes <kevinstokes@Kevins-MacBook-Pro.local>
2019-07-20 12:06:35 -0400

    merging onto new branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import Form from './components/Form';
+import Nav from "./components/Nav";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
+      <Hero />
       <Form />
+      <Footer />
     </div>
   );
 }
