b14e63bffc76a0c2cf96a14b68070277bda9dc9c
tblasberg <terena.blasberg@gmail.com>
2019-07-31 20:33:24 -0400

    google drive in progress - embedded

--- client/src/components/GDrive/index.js
+++ client/src/components/GDrive/index.js
@@ -1,14 +1,38 @@
 /* eslint-disable jsx-a11y/iframe-has-title */
 import React, {Component} from "react";
+var axios = require("axios");
+
 
 class GDrive extends Component {
+    state = {
+      gDrive: ""
+    }
 
+    // https://drive.google.com/embeddedfolderview?id=1cse1H3HnHNAtH-2la6UG7FnbMXHuwH8e#grid
  
+    componentDidMount() {
+        axios({
+          method: "get",
+          url: "/api/projects/{idprojects}/gdrive",
+          }
+        )
+          .then(response => {
+            if (response && response.data) {
+              this.setState({ gDrive: response.data });
+            }
+          })
+          .catch(error => console.log(error));
+      };
 
-    render(){
+      // --------------------------------------------
+      
+      
+      render(){
+        const {gDrive} = this.state;
+
         return (
             <div>
-                <iframe src="https://drive.google.com/open?id=1cse1H3HnHNAtH-2la6UG7FnbMXHuwH8e" height="500" width="600" />         
+                <iframe src={gDrive} height="500" width="600" />         
             </div>           
         )
     }
